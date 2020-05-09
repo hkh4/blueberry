735e47d6787d437b62e63191782b0fd06547d177
Hugo Hua <hugokingstonhua@gmail.com>
2020-05-08 21:16:23 -0400

    evaluated options

--- lang/Library.fs
+++ lang/Library.fs
@@ -1,5 +1,6 @@
 module CS334
 
+open System
 open Parser
 (*
 <expr>             ::= <option>
@@ -43,34 +44,244 @@ std = strum down
 p = parens
 plu = pluck up
 pld = pluck down
-g = grace note
+gr = grace note
 har = harmonic
 sl = slide
 si = slide in
 
 *)
 type Pitch =
-| A | ASharp | AFlat | B | BSharp | BFlat | C | CSharp | CFlat | D | DSharp | DFlat | E | ESharp | EFlat | F | FSharp | FFlat | G | GSharp | GFlat
+| A | ASharp | AFlat | ANat | B | BSharp | BFlat | BNat | C | CSharp | CFlat | CNat | D | DSharp | DFlat | DNat | E | ESharp | EFlat | ENat | F | FSharp | FFlat | FNat | G | GSharp | GFlat | GNat
 
-type Simple =
-| Singlesimple of string * Pitch
-| Restsimple
+type Property = | Gr | Sls | Sle | Stu | Std | P | Plu | Pld | Har | Sl | Si
+
+type simple =
+| SingleSimple of int * Pitch * Property List
+| RestSimple
+
+type RhythmNumber =
+| X1 | X2 | X4 | X8 | X16 | X32 | X64
+
+type Rhythm = RhythmNumber * int          //int is the number of dots
+
+type complex =
+| SingleComplex of int * Pitch * Rhythm * Property List
+| RestComplex of Rhythm
 
 type Note =
-| Simple | Complex | Group | Tuplet
-
-type Type =
-
-type Option =
-| Type | Time | Key
+| Simple of simple
+| Complex of complex
+| Group
+| Tuplet
 
 type Expr =
-| Option
-| Measure of Note List
+| ScoreOption of string * string
+| Measure of int * Note List
+
+
+// Helpers
+let pword = pmany1 (psat (fun c -> (c <> ' ') && (c <> '\n'))) |>> stringify <!> "word"
+let pdigit2 = pdigit |>> (fun a -> System.Char.GetNumericValue a |> int) <!> "pdigit2"
+
+// ****************  PARSE OPTIONS ********************
+
+// '-' then word, then space, then another word, then space
+let typeoption = pseq (pleft pword pws1 ) pword (fun (a,b) -> (a,b)) <!> "typeoption"
+let singleOption = pbetween (pchar '-') pws1 typeoption |>> ScoreOption <!> "singleOption"
+let option = pleft (pmany0 singleOption) pws0 <!> "option"
+
+// **************** PARSE MEASURES ********************
+
+// start with a measure number and then :
+let measureNumber = pleft pdigit2 (pchar ':') <!> "Measure number"
+
+///////////// NOTE ///////////////
+
+//pitches
+let csharp = pstr "c#" |>> (fun _ -> CSharp) <!> "csharp"
+let cflat = pstr "cb" |>> (fun _ -> CFlat) <!> "cflat"
+let cnat = pstr "cn" |>> (fun _ -> CNat) <!> "cnat"
+let dsharp = pstr "d#" |>> (fun _ -> DSharp) <!> "dsharp"
+let dflat = pstr "db" |>> (fun _ -> DFlat) <!> "dflat"
+let dnat = pstr "dn" |>> (fun _ -> DNat) <!> "dnat"
+let esharp = pstr "e#" |>> (fun _ -> ESharp) <!> "esharp"
+let eflat = pstr "eb" |>> (fun _ -> EFlat) <!> "eflat"
+let enat = pstr "en" |>> (fun _ -> ENat) <!> "enat"
+let fsharp = pstr "f#" |>> (fun _ -> FSharp) <!> "fsharp"
+let fflat = pstr "fb" |>> (fun _ -> FFlat) <!> "fflat"
+let fnat = pstr "fn" |>> (fun _ -> FNat) <!> "fnat"
+let gsharp = pstr "g#" |>> (fun _ -> GSharp) <!> "gsharp"
+let gflat = pstr "gb" |>> (fun _ -> GFlat) <!> "gflat"
+let gnat = pstr "gn" |>> (fun _ -> GNat) <!> "gnat"
+let asharp = pstr "a#" |>> (fun _ -> ASharp) <!> "asharp"
+let aflat = pstr "ab" |>> (fun _ -> AFlat) <!> "aflat"
+let anat = pstr "an" |>> (fun _ -> ANat) <!> "anat"
+let bsharp = pstr "b#" |>> (fun _ -> BSharp) <!> "bsharp"
+let bflat = pstr "bb" |>> (fun _ -> BFlat) <!> "bflat"
+let bnat = pstr "bn" |>> (fun _ -> BNat) <!> "bnat"
+let a = pchar 'a' |>> (fun _ -> A) <!> "a"
+let b = pchar 'b' |>> (fun _ -> B) <!> "b"
+let c = pchar 'c' |>> (fun _ -> C) <!> "c"
+let d = pchar 'd' |>> (fun _ -> D) <!> "d"
+let e = pchar 'e' |>> (fun _ -> E) <!> "e"
+let f = pchar 'f' |>> (fun _ -> F) <!> "f"
+let g = pchar 'g' |>> (fun _ -> G) <!> "g"
+
+let pitch = csharp <|> cflat <|> cnat <|> dsharp <|> dflat <|> dnat <|> esharp <|> eflat <|> enat <|> fsharp <|> fflat <|> fnat <|> gsharp <|> gflat <|> gnat <|> asharp <|> aflat <|> anat <|> bsharp <|> bflat <|> bnat <|> a <|> b <|> c <|> d <|> e <|> f <|> g <!> "pitch"
+
+//properties
+let sls = pstr "sls" |>> (fun _ -> Sls) <!> "sls"
+let sle = pstr "sle" |>> (fun _ -> Sle) <!> "sle"
+let stu = pstr "stu" |>> (fun _ -> Stu) <!> "stu"
+let std = pstr "std" |>> (fun _ -> Std) <!> "std"
+let plu = pstr "plu" |>> (fun _ -> Plu) <!> "plu"
+let pld = pstr "pld" |>> (fun _ -> Pld) <!> "pld"
+let har = pstr "har" |>> (fun _ -> Har) <!> "har"
+let gr = pstr "gr" |>> (fun _ -> Gr) <!> "gr"
+let sl = pstr "sl" |>> (fun _ -> Sl) <!> "sl"
+let si = pstr "si" |>> (fun _ -> Si) <!> "si"
+let p = pstr "p" |>> (fun _ -> P) <!> "p"
+
+let property = pright (pchar '/') (sls <|> sle <|> stu <|> std <|> plu <|> pld <|> har <|> gr <|> sl <|> si <|> p) <!> "property"
+let properties = pmany0 property <!> "properties"
+
+//rhythms
+let x64 = pstr "64" |>> (fun _ -> X64) <!> "64"
+let x32 = pstr "32" |>> (fun _ -> X32) <!> "32"
+let x16 = pstr "16" |>> (fun _ -> X16) <!> "16"
+let x8 = pstr "8" |>> (fun _ -> X8) <!> "8"
+let x4 = pstr "4" |>> (fun _ -> X4) <!> "4"
+let x2 = pstr "2" |>> (fun _ -> X2) <!> "2"
+let x1 = pstr "1" |>> (fun _ -> X1) <!> "1"
+
+let dot = pmany0 (pchar '.') |>> (fun list -> list.Length) <!> "dot"
+
+let rhythm = pseq (x64 <|> x32 <|> x16 <|> x8 <|> x4 <|> x2 <|> x1) dot (fun (a,b) -> (a,b)) |>> Rhythm <!> "Rhythm"
+
+///// SIMPLE //////
+
+//SINGLESIMPLE
+let singlesimple = pseq pdigit2 (pseq pitch properties (fun (a,b) -> (a,b))) (fun (a,(b,c)) -> (a,b,c)) |>> SingleSimple <!> "singlesimple"
+
+//RESTSIMPLE
+let restsimple = pchar 'r' |>> (fun _ -> RestSimple) <!> "restsimple"
+
+// Simple
+let simple = singlesimple <|> restsimple |>> Simple <!> "simple"
+
+///// COMPLEX //////
+
+//SINGLECOMPLEX
+let singlecomplex = pseq pdigit2 (pseq pitch (pseq rhythm properties (fun (a,b) -> (a,b))) (fun (a,(b,c)) -> (a,b,c))) (fun (a,(b,c,d)) -> (a,b,c,d)) |>> SingleComplex <!> "singlecomplex"
+
+//RESTCOMPLEX
+let restcomplex = pright (pchar 'r') rhythm |>> RestComplex <!> "restcomplex"
+
+// Complex
+let complex = singlecomplex <|> restcomplex |>> Complex <!> "complex"
+
+// Note
+let note = pright pws1 (complex <|> simple) <!> "note"
+
+// Measure
+let measure1 = pseq measureNumber (pmany1 note) (fun (a,b) -> (a,b)) |>> Measure <!> "measure!"
+
+
+let expr = pseq option (pmany0 (pleft measure1 pws1)) (fun (a,b) -> (a,b)) <!> "expr"
+
+let grammar = pleft expr (pleft pws0 peof) <!> "grammar"
+
+let parse input =
+   let input' = prepare input
+   match grammar input' with
+   | Success(res,_) -> Some res
+   | Failure(pos,rule) ->
+      printfn "Invalid expression"
+      let msg = sprintf "Cannot parse input at pos %d in rule '%s':" pos rule
+      let diag = diagnosticMessage 20 pos input msg
+      printf "%s" diag
+      None
+
+
+
+// ******************* EVAL *********************
+
+//Data structures
+type optionsRecord = {
+   Type: string
+   Time: string
+   Key: string
+}
+
+// ******************* EVALUATE OPTION **********************
+
+let parseOptions (a : Expr) (optionsR : optionsRecord) =
+   match a with
+   | ScoreOption(key: string,value: string) when key = "type" ->
+      Some({optionsR with Type = value})
+   | ScoreOption(key: string,value: string) when key = "key" ->
+      Some({optionsR with Key = value})
+   | ScoreOption(key: string,value: string) when key = "time" ->
+      Some({optionsR with Time = value})
+   | _ ->
+      printfn "Invalid option! Valid options are type, key, and time"
+      None
+
+let rec evalOption o optionsR=
+   match o with
+   | [] -> Some(optionsR)
+   | head::tail ->
+      match (parseOptions head optionsR) with
+         | Some(r) -> evalOption tail r
+         | None -> None
+
+
+
+
+// ******************** EVALUATE MEASURE *******************
+
+
+// Eval
+let eval ast =
+   let (o,m) = ast
+   let optionsR = {Type = "tab"; Time = "4-4"; Key = "c"}
+   printfn "%A" o
+   printfn "%A" m
+   match (evalOption o optionsR) with
+   | Some(r) ->
+      printfn "%A" r
+      Some(r)
+   | None ->
+      None
 
 
 
 
 
-let expr, exprImpl = recparser()
-exprImple := pseq (pchar 'a') pws1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
--- lang/Program.fs
+++ lang/Program.fs
@@ -1,9 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
 open Parser
-open cs334 
+open CS334
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+   if (Array.length argv = 0) then
+      1
+   else
+      let fileName = argv.[0]
+      let fileText = System.IO.File.ReadAllText fileName
+      printfn "%A" fileText
+      match parse fileText with
+      | Some ast ->
+         match (eval ast) with
+         | Some(_) -> 0
+         | None -> 1
+      | None ->
+         printfn "Error!"
+         0
